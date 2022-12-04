12b86e58ae336bbe389bddaebef5d20b33680d2b
Nakul Bajaj <bajaj.nakul@gmail.com>
2022-12-04 16:26:51 +1100

    Updated the api gateway url

--- test_lambda.py
+++ test_lambda.py
@@ -1,7 +1,8 @@
 import requests 
 
-url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+#url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+url = "https://129j0j70wa.execute-api.us-east-2.amazonaws.com/test"
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
-result = requests.post(url=url, json=data).json()
+result = requests.post(url=f"{url}/predict-clothing", json=data).json()
 print(result)
